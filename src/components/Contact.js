6b4ad33840224e803da77a39eac19bf1fbb306be
Alaa Desouky <alaa.desouky33@gmail.com>
2018-12-20 20:47:13 +0200

    working with state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,38 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-class Contact extends React.Component {
-  // constructor(){
-  //   super();
-  //   this.state = {
-  //     text: 'This is the default state',
-  //     num: 5
-  //   }
-  // }
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
 
-  // update(e) {
-  //   this.setState({text: e.target.value})
-  // }
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
+
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
 
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}{" "}
+          <i
+            onClick={this.onShowClick}
+            className="fas fa-sort-down"
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            onClick={this.onDeleteClick}
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+          />
+        </h4>
+        {showContactInfo && (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        )}
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 
 import "./styles.css";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <Contact
-            name="John Doe"
-            email="jdoe@gmail.com"
-            phone="555-555-5555"
-          />
-          <Contact
-            name="Karen Smith"
-            email="karen@gmail.com"
-            phone="444-444-4444"
-          />
+          <Contacts />
         </div>
       </div>
     );
